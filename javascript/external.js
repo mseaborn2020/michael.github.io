c514b1e436cadc49837d50b9358f7302c9745e55
mseaborn2020 <mseaborn2020@gmail.com>
2025-09-18 20:36:02 -0500

    removed name list

--- javascript/external.js
+++ javascript/external.js
@@ -1,7 +1,2 @@
-arr = ["Mael", "Eric", "Randall", "James"];
-for (var i=0; i < arr.length; i++)
-{
-  document.writeln(arr[i],"<br>");
-}
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name)
