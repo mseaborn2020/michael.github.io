cd2a626db66ba6a4e310585436022d284ab01424
mseaborn2020 <mseaborn2020@gmail.com>
2025-09-25 20:27:25 -0500

    Defined a function for the welcome message.

--- javascript/external.js
+++ javascript/external.js
@@ -1,2 +1,13 @@
-user_name = window.prompt("Please enter your name", "Type your name here");
+/* user_name = window.prompt("Please enter your name", "Type your name here");
 document.getElementById("welcome-heading").textContent ="Hello,Welcome " + user_name +"!";
+*/
+function welcome()
+{
+  let a = "Please enter your name.";
+  let b = "Type your name here.";
+  //A prompt box is used to prompt users to input a value before entering a page
+  user_name = window.prompt(a, b);
+  message = "<h1>Hello, welcome to my webpage," + user_name + "!</h1>"
+  return message
+}
+document.write(welcome());
